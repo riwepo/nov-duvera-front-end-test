68287eb70cb4cefd58658f862a1b1a17c00d0509
Richard Potter <richardpotter.au@gmail.com>
2023-09-21 09:56:23 +1000

    skeleton of page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h1>hello</h1>;
+  return (
+    <div className="bg-white h-screen grid grid-rows-[1fr_auto_1fr] grid-cols-[1fr_auto_1fr]">
+      <div className="row-start-2 col-start-2 bg-transparent flex flex-col items-center">
+        <h1 className="text-black py-10 font-bold text-xl">QUADRA TEST</h1>
+        <div className="bg-gray-100 rounded-md p-10 font-normal text-2xl">
+          <p>6515646</p>
+        </div>
+      </div>
+    </div>
+  );
 }
