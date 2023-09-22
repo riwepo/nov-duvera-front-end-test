df7c140d3a74f943ff7c47ffadc8cf873685bae3
Richard Potter <richardpotter.au@gmail.com>
2023-09-23 09:18:37 +1000

    rename to trending number

---
+++ app/components/TrendingNumber.tsx
@@ -0,0 +1,20 @@
+import getTailwindColorClass from "../lib/getTailwindColorClass";
+
+interface TrendingNumberProps {
+  older: number;
+  newer: number;
+  className?: string;
+}
+
+// display the newer number
+// if the number is unchanged, display in white
+// if the number has increased, display in green
+// if the number has decreased, display in red
+
+const TrendingNumber: React.FC<TrendingNumberProps> = function (props) {
+  const colorClass = getTailwindColorClass(props.older, props.newer);
+  return (
+    <p className={`text-${colorClass} ${props.className}`}>{props.newer}</p>
+}
+
+export default TrendingNumber;
