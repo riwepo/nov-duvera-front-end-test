66f7f7ec6a4f7fd27b13ac387d58ff97d7e0d850
Richard Potter <richardpotter.au@gmail.com>
2023-09-23 09:17:21 +1000

    change state use use TrandingNumber

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,35 @@
 
 import { useState } from "react";
 
-import DynamicNumber from "./components/DynamicNumber";
+import TrendingNumber from "./components/TrendingNumber";
 
 import useWebSocketNumberListener from "./hooks/useWebSocketNumberListener";
 
 export default function Home() {
-  const [currentNumber, setCurrentNumber] = useState<number>(0);
+  // store the older number at index 1
+  // store the newer number at index 0
+  const [currNumbers, setCurrNumbers] = useState<number[]>([0, 0]);
   const newNumberHandler = (newNumber: number) => {
-    setCurrentNumber(newNumber);
+    setCurrNumbers((state) => {
+      // create a new array with the numbers shuffled
+      const newState = [newNumber, state[0]];
+      return newState;
+    });
   };
+
+  // list for a new random number on a web socket
   useWebSocketNumberListener("ws://localhost:8080", newNumberHandler);
 
   return (
-    <div className="bg-white h-screen grid grid-rows-[1fr_auto_1fr] grid-cols-[1fr_auto_1fr]">
+    <div className="bg-black h-screen grid grid-rows-[1fr_auto_1fr] grid-cols-[1fr_auto_1fr]">
       <div className="row-start-2 col-start-2 bg-transparent flex flex-col items-center">
-        <h1 className="text-black py-10 font-bold text-xl">QUADRA TEST</h1>
-        <div className="bg-gray-100 rounded-md p-10 font-normal text-2xl">
-          <DynamicNumber number={currentNumber} colorClass="text-green-500" />
+        <h1 className="text-white py-10 font-bold text-xl">QUADRA TEST</h1>
+        <div className="bg-gray-500 rounded-md p-10 font-normal text-2xl">
+          <TrendingNumber
+            older={currNumbers[1]}
+            newer={currNumbers[0]}
+            className="font-mono"
+          />
         </div>
       </div>
     </div>
