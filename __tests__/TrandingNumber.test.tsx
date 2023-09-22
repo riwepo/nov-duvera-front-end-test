ef2a75a1a7581085ded53ee4e864a5d0f5f68996
Richard Potter <richardpotter.au@gmail.com>
2023-09-23 09:19:50 +1000

    rename

--- __tests__/TrandingNumber.test.tsx
+++ __tests__/TrandingNumber.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import DynamicNumber from "../app/components/DynamicNumber";
+import DynamicNumber from "../app/components/TrendingNumber";
 import "@testing-library/jest-dom";
 
 describe("DynamicNumber component test suite", () => {
