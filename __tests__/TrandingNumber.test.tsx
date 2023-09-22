7c0418a8181c95f1ad338f70a88720f9cc634021
Richard Potter <richardpotter.au@gmail.com>
2023-09-23 09:27:29 +1000

    updated first test

--- __tests__/TrandingNumber.test.tsx
+++ __tests__/TrandingNumber.test.tsx
@@ -1,14 +1,13 @@
 import { render, screen } from "@testing-library/react";
-import DynamicNumber from "../app/components/TrendingNumber";
+import TrendingNumber from "../app/components/TrendingNumber";
 import "@testing-library/jest-dom";
 
-describe("DynamicNumber component test suite", () => {
-  const number = 1234;
-  const colorClass = "text-green-500";
-  it("number with classes", () => {
-    render(<DynamicNumber number={number} colorClass={colorClass} />);
-    const numberElement = screen.getByText(number);
-    expect(numberElement).toBeInTheDocument();
-    expect(numberElement).toHaveClass(colorClass);
+describe("TrendingNumber component test suite", () => {
+  const older = 1234;
+  const newer = 5678;
+  it("displays the newer number", () => {
+    render(<TrendingNumber older={older} newer={newer} />);
+    const newerNumberElement = screen.getByText(newer);
+    expect(newerNumberElement).toBeInTheDocument();
   });
 });
