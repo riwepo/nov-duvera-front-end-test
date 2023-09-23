9f921c8950de56b16ffe77a5f4c1ec80a2a809ee
Richard Potter <richardpotter.au@gmail.com>
2023-09-23 15:02:02 +1000

    added tests

---
+++ __tests__/getTrend.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { Trend, getTrend } from "../app/lib/getTrend";
+
+describe("getTrend component test suite", () => {
+  it("returns unknown if older is null", () => {
+    const older = null;
+    const newer = 1234;
+    const result = getTrend(older, newer);
+    expect(result).toBe(Trend.Unknown);
+  });
+  it("returns unknown if newer is null", () => {
+    const older = 1234;
+    const newer = null;
+    const result = getTrend(older, newer);
+    expect(result).toBe(Trend.Unknown);
+  });
+  it("returns increasing if newer > older", () => {
+    const older = 1;
+    const newer = 2;
+    const result = getTrend(older, newer);
+    expect(result).toBe(Trend.Increasing);
+  });
+  it("returns decreasing if newer < older", () => {
+    const older = 2;
+    const newer = 1;
+    const result = getTrend(older, newer);
+    expect(result).toBe(Trend.Decreasing);
+  });
+  it("returns steady if newer = older", () => {
+    const older = 1;
+    const newer = 1;
+    const result = getTrend(older, newer);
+    expect(result).toBe(Trend.Steady);
+  });
+});
