de2400c51db8614159b5e527ca26f639a49fa5e4
Richard Potter <richardpotter.au@gmail.com>
2023-09-23 15:48:47 +1000

    used constants

--- __tests__/useWebSocketNumberListener.test.tsx
+++ __tests__/useWebSocketNumberListener.test.tsx
@@ -4,15 +4,17 @@ import "@testing-library/jest-dom";
 import useWebSocketNumberListener from "@/app/hooks/useWebSocketNumberListener";
 
 import config from "../config.json";
+import { NUMBER_SERVER_PERIOD_MS } from "@/app/lib/constants";
 const webSocketUrl = config.SERVER_URL;
 
 describe("TrendingNumber component test suite", () => {
   it("call back gets called if we wait a little", async () => {
+    // note this test depends on the number server running
     const onNewNumber = jest.fn();
     renderHook(() => {
       useWebSocketNumberListener(webSocketUrl, onNewNumber);
     });
-    await new Promise((r) => setTimeout(r, 200));
+    await new Promise((r) => setTimeout(r, NUMBER_SERVER_PERIOD_MS * 2));
     expect(onNewNumber).toBeCalled();
   });
   it("throws if we pass in a url that doesn't start with 'ws'", async () => {
