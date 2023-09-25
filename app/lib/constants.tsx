e6c7a23ddc1a712bdf8d6ba4e8346aa70e6bb747
Richard Potter <richardpotter.au@gmail.com>
2023-09-26 08:57:22 +1000

    comments

--- app/lib/constants.tsx
+++ app/lib/constants.tsx
@@ -1,2 +1,5 @@
+// number of digits output by the server on the socket
 export const NUMBER_SERVER_DIGITS = 7;
+
+// update period of the server on the socket
 export const NUMBER_SERVER_PERIOD_MS = 100;
