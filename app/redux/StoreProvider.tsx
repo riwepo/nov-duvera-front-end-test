bbae1fd8ebcc478935e6168a6082d19d79c03125
Richard Potter <richardpotter.au@gmail.com>
2023-09-25 13:18:36 +1000

    neaten comments

--- app/redux/StoreProvider.tsx
+++ app/redux/StoreProvider.tsx
@@ -3,12 +3,8 @@
 import store from "./store";
 import { Provider } from "react-redux";
 
-/**
- * when used to wrap components it provides the redux store
- * we implement this separately because we only want it to be defined on the client
- * @param param0
- * @returns
- */
+// when used to wrap components it provides the redux store
+// we implement this separately because we only want it to be defined on the client
 export function StoreProvider({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
 }
