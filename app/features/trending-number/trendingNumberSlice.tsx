45117d297239bd3e7c3096f8d8bda7fa6348c787
Richard Potter <richardpotter.au@gmail.com>
2023-09-25 12:31:02 +1000

    set types according to redux docs

--- app/features/trending-number/trendingNumberSlice.tsx
+++ app/features/trending-number/trendingNumberSlice.tsx
@@ -1,15 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+import type { RootState } from "../../redux/store";
 
 interface ITrendingNumberState {
   prevNumber: number | null;
   currNumber: number | null;
 }
 
-interface ITrendingNumberAction {
-  type: string;
-  payload: number;
-}
-const initialState = {
+const initialState: ITrendingNumberState = {
   prevNumber: null,
   currNumber: null,
 };
@@ -18,8 +16,8 @@ const trendingNumberSlice = createSlice({
   name: "trendingNumber",
   initialState: initialState,
   reducers: {
-    nextNumber(state, action) {
-      // we are allowd to mutate state here
+    nextNumber(state, action: PayloadAction<number>) {
+      // we are allowed to mutate state here
       // RTK makes it immuatable behind the scenes
       (state.prevNumber = state.currNumber),
         (state.currNumber = action.payload);
@@ -29,24 +27,4 @@ const trendingNumberSlice = createSlice({
 
 export default trendingNumberSlice.reducer;
 export const nextNumber = trendingNumberSlice.actions.nextNumber;
-
-// export default function trendingNumberReducer(
-//   currentState: ITrendingNumberState = trendingNumberInitialState,
-//   action: ITrendingNumberAction
-// ): ITrendingNumberState {
-//   switch (action.type) {
-//     case "trendingNumber/next":
-//       const newState: ITrendingNumberState = {
-//         ...currentState,
-//         prevNumber: currentState.currNumber,
-//         currNumber: action.payload,
-//       };
-//       return newState;
-//     default:
-//       return currentState;
-//   }
-// }
-
-// export function nextNumber(n: number): ITrendingNumberAction {
-//   return { type: "trendingNumber/next", payload: n };
-// }
+export const selectTrandingNumber = (state: RootState) => state.trendingNumber;
