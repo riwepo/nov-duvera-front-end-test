5853ff4c6df0690a39b3ae97636d1b6757a10847
Richard Potter <richardpotter.au@gmail.com>
2023-09-25 12:32:22 +1000

    pass in message callback

--- app/redux/socketMiddleware.tsx
+++ app/redux/socketMiddleware.tsx
@@ -1,19 +1,23 @@
 import Socket from "../lib/socket";
 import config from "../../config.json";
-import { nextNumber } from "../features/trending-number/trendingNumberSlice";
 
-/***
- * thanks to https://www.taniarascia.com/websockets-in-redux/
- */
-export function socketMiddleware(socket: Socket) {
-  return function (params) {
+// here we implement some redux middleware
+// we have a WebSocket
+// this middleware resonds to actions 'connect' and 'disconnect'
+// on receipt of a message, it calls the onMessage callback provided
+// thanks to https://www.taniarascia.com/websockets-in-redux/
+export function socketMiddleware(
+  socket: Socket,
+  onMessage: (data: number) => void
+) {
+  return function () {
     return function (next) {
       return function (action: { type: string }) {
         switch (action.type) {
           case "socket/connect":
             socket.connect(config.SERVER_URL);
-            socket.onMessage((event: MessageEvent<number>) => {
-              params.dispatch(nextNumber(event.data));
+            socket.addMessageListener((event: MessageEvent<number>) => {
+              onMessage(event.data);
             });
             break;
 
